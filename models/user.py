938ba31ceac03da86e74beaf452797005feed942
ela-hidri <elahidri.eh@gmail.com>
2023-09-18 15:30:38 -0700

    fixing user

--- models/user.py
+++ models/user.py
@@ -7,7 +7,7 @@ from sqlalchemy import Column, String, Integer
 class User(BaseModel):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = from sqlalchemy import Column, String, Integer
-    password = from sqlalchemy import Column, String, Integer
-    first_name = from sqlalchemy import Column, String, Integer
-    last_name = from sqlalchemy import Column, String, Integer
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
