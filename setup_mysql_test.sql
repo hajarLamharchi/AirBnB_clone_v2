b0d65604e77fb58ba4d80fa2fcc44a4646ecb99e
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-18 18:19:08 +0000

    task4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- This script prepares a Mysql server for the project
+
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'hbnb_test_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
