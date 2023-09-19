aeb19dbd8890c89b25e0453b2434ce80a3d4f987
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-09-19 15:08:05 +0000

    adding/modifiyng Place, User, City

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from sqlalchemy import Column, Integer, String, ForeignKey
 from models.base_model import Base
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -16,3 +17,7 @@ class City(BaseModel, Base):
     name = Column(String(128),
                   nullable=False
     )
+    places = relationship('Place',
+                          backref='cities',
+                          cascade='all,delete-orphan',
+                          uselist=True)
