98d277e139fd582eff9bed70e5ad3489c8955be6
ela-hidri <elahidri.eh@gmail.com>
2023-09-20 15:04:57 -0700

    fixing amenity

--- models/place.py
+++ models/place.py
@@ -38,7 +38,7 @@ class Place(BaseModel, Base):
                            cascade='all,delete-orphan', uselist=True)
 
     amenities = relationship('Amenity', secondary=place_amenity,
-                             viewonly=False, backref="Place")
+                             viewonly=False)
     amenity_ids = []
 
     @property
