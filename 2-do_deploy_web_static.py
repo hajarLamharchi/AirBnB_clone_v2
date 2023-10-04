c195c9a17fe854eb0cec2d7f4bde9dcf06354c7d
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-04 20:21:37 +0000

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,19 +17,23 @@ def do_deploy(archive_path):
     try:
         put('archive_path', '/tmp/')
 
-        archive_file = archive_path.split('/')[-1]
-        folder = archive_file[:-4]
+        archive = archive_path.split('/')[-1]
+        folder = archive[:-4]
 
         run('mkdir -p /data/web_static/releases/{}/'.format(folder))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_file, folder))
+        run(
+            'tar -xzf /tmp/{} '
+            '-C /data/web_static/releases/{}/'.format(archive, folder)
+            )
 
-        run ('rm /tmp/{}'.format(archive_file))
+        run('rm /tmp/{}'.format(archive))
 
         run('rm - rf /data/web_static/current')
 
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder))
-        
+        run(
+            'ln -s /data/web_static/releases/{}/ '
+            '/data/web_static/current'.format(folder)
+            )
         return True
     except Exception as e:
         return False
-
