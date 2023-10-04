36cce9cf5ce46d0bf1c8d5e21109da084a0c7094
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-04 22:20:59 +0000

    task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,9 @@
 from fabric.api import run, put, env, sudo
 from os.path import exists
 
-env.hosts = ['52.86.213.205', '52.91.119.144']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.hosts = ["52.86.213.205", "52.91.119.144"]
+env.user = "ubuntu"
+env.key_filename = "~/.ssh/id_rsa"
 
 
 def do_deploy(archive_path):
@@ -15,31 +15,24 @@ def do_deploy(archive_path):
         return False
 
     try:
-        put(archive_path, '/tmp/')
+        put(archive_path, "/tmp/")
 
-        archive = archive_path.split('/')[-1]
+        archive = archive_path.split("/")[-1]
         folder = archive[:-4]
 
-        run('mkdir -p /data/web_static/releases/{}/'.format(folder))
+        run("mkdir -p /data/web_static/releases/{}/".format(folder))
         run(
-            'tar -xzf /tmp/{} '
-            '-C /data/web_static/releases/{}/'.format(archive, folder)
+            "tar -xzf /tmp/{} "
+            "-C /data/web_static/releases/{}/".format(archive, folder)
             )
 
-        run('rm /tmp/{}'.format(archive))
+        run("rm /tmp/{}".format(archive))
 
-        if exists('/data/web_static/releases/{}/web_static'.format(folder)):
-            run(
-                'mv /data/web_static/releases/{}/web_static/* '
-                '/data/web_static/releases/{}/'.format(folder, folder)
-                )
-            run('rm -rf /data/web_static/releases/{}/web_static'.format(folder))
-
-        run('rm -rf /data/web_static/current')
+        run("rm -rf /data/web_static/current")
 
         run(
-            'ln -s /data/web_static/releases/{}/ '
-            '/data/web_static/current'.format(folder)
+            "ln -s /data/web_static/releases/{}/ "
+            "/data/web_static/current".format(folder)
             )
         return True
     except Exception as e:
