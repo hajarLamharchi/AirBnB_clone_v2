6b1585e6e4d2361d2773d7379b4b91dfc9c70ced
hajarLamharchi <hajar.lamharchi@gmail.com>
2023-10-22 22:16:08 +0000

    fix task8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -64,7 +64,7 @@ def even_odd(n):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """displays an HTML page"""
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
